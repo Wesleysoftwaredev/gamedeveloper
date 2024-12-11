37d3db8d0331eab5f81e906954c690b4f0a945e7
Wesleysoftwaredev <aghayedow@gmail.com>
2024-12-11 18:55:55 +0000

    Add files via upload

---
+++ dictionary.py
@@ -0,0 +1,8 @@
+dict1 = {"burger" : "beef",
+         "turkey" : "chicken",
+         "chips"  : "potato",
+         "fried rice" : "prawn"}
+#acsessing a value
+print (dict1["burger"])
+print (dict1.keys())
+print (dict1.values())
