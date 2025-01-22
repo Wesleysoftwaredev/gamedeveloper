24d8c80e560e7405860720097ff16f7573cf73f9
Wesleysoftwaredev <aghayedow@gmail.com>
2025-01-22 18:25:07 +0000

    Add files via upload

--- shapes.py
+++ shapes.py
@@ -1,23 +1,25 @@
 import pgzrun, random
-WIDTH = 500
-HEIGHT = 500
+WIDTH = 750
+HEIGHT = 550
 
 def draw():
-    w=500
-    y=100
+    z=550
+    x=150
     screen.fill("black")
-    for i in range(20):
-
+    for i in range(25):
         r=random.randint(0, 255)
         g=random.randint(0,255)
         b=random.randint(0,255)
 
-        obj = Rect(0,0,w,y)
-        obj.center=250,250
-        screen.draw.rect(obj, (r, g, b))
-        w -= 20
-        y += 20
+        obj = Rect(0,0,z,x)
+        obj.center=300,300
+        #screen.draw.rect(obj, (r,g,b))
+        z -= 20
+        x += 20
+    screen.draw.filled_circle((375,225),50,"red")
 
+    screen.draw.line((375,225),(415,285),"yellow")
 
-
+    screen.draw.text(("Skibidi Toilet"),(375,225),fontsize=45,color="yellow")
 pgzrun.go()
+
