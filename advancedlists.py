bb7b042fe15a3a3ee5ec2d3471d4860355b0baa8
Wesleysoftwaredev <aghayedow@gmail.com>
2025-02-12 18:44:11 +0000

    Add files via upload

--- advancedlists.py
+++ advancedlists.py
@@ -5,7 +5,7 @@ for i in AT1:
     
 #creating a 2d list
 
-MAT1 = [[1,2,3,11],[4,5,6,12],[7,8,9,13]]
+"""MAT1 = [[1,2,3,11],[4,5,6,12],[7,8,9,13]]
 #number of rows
 print(len(MAT1))
 #number of collums
@@ -21,4 +21,20 @@ for i in MAT1[0]:
 for i in range(len(MAT1)):
     for j in range(len(MAT1[0])):
         print(MAT1[i][j],end=" ")
-    print()
+    print()"""
+
+#markssegragation
+
+L1 = []
+L2 = []
+L3 = []
+for i in range(20):
+    mark2 = int(input("Please enter your test mark:"))
+    if mark2<=30:
+        L1.append(mark2)
+    elif mark2>30 and mark2<=69:
+        L2.append(mark2)
+    elif mark2>=70:
+        L3.append(mark2)
+Matrix = [L1,L2,L3]
+print(Matrix)
